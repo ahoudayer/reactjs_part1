8b058e32779666c932f2e64515b056472b50c2ab
arnahou <arnaud.houdayer@darva.com>
2025-09-29 12:21:08 +0200

    exo 4

--- 05_arrayOperations.js
+++ 05_arrayOperations.js
@@ -10,8 +10,9 @@
  * 
   */
 
-const multiplyByTwo = null;
-
+const multiplyByTwo = [1,3,5,7];
+let a = multiplyByTwo.map((x) => x * 2);
+console.log("multiplyByTwo [1,3,5,7] : "+a);
 
 /**
  * Utiliser la fonction .filter sur le tableau passé en paramètre
@@ -23,7 +24,13 @@ const multiplyByTwo = null;
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const filterNameStartByA = null;
+const filterNameStartByA = ["spray", "elite", "exuberant", "destruction", "present"];
+const result = filterNameStartByA.filter((a) => a.includes("a"));
+console.log("filterNameStartByA [A] : "+result);
+// Expected output: Array ["exuberant", "destruction", "present"]
+
+
+
 
 /**
  * Utiliser la fonction .reduce sur le tableau passé en paramètre
@@ -35,7 +42,27 @@ const filterNameStartByA = null;
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const sum = null;
+const sum = [1, 2, 3, 4];
+let a2 = [];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = sum.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log("sum with reduce [1, 2, 3, 4] : " + sumWithInitial);
+// Expected output: 10
+
+let sumWithInitial2 = a2 => (
+  a2.reduce(
+  (acc, i) => acc + i,
+    0
+  )
+) 
+console.log("sum with reduce [1,2,6,12] : " + sumWithInitial2([1,2,6,12]));
+
 
 /**
  * Utiliser la fonction .find sur le tableau passé en paramètre
@@ -56,7 +83,26 @@ const sum = null;
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const findUserById = null;
+const array = [5, 12, 8, 130, 44];
+const found = array.find((element) => element > 10);
+console.log(found);
+
+let arryUsers = [
+ {id: 1, name: 'John'},
+ {id: 2, name: 'Doe'},
+ {id: 3, name: 'Foo'},
+ {id: 4, name: 'Bar'},
+];
+
+const findUserById = (a,b) => a.find(a=>a.id ===b);
+console.log(findUserById(arryUsers, 3));
+
+
+
+// Expected output: 12
+
+
+
 
 
 module.exports = {multiplyByTwo, filterNameStartByA, sum, findUserById};
